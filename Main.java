a2a782b6bf94abbc6225bb603a47b2f15ab3de94
ARUN-M-S <arunms96official@gmail.com>
2025-03-08 14:03:34 +0530

    java basics

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+public class Main{
+    int a =100;
+    long b =200L;
+
+    public static void main(String[] args) {
+     
+        Main obj = new Main();
+      System.out.println("  * * *\t * * * ");
+      System.out.println(" *     *      *");
+
+    }
+
+}
