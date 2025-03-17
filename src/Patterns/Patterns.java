fb85f415c4582362bc35855576aa78e126cfee51
ARUN-M-S <arunms96official@gmail.com>
2025-03-17 16:16:14 +0530

    Number  patterns

--- src/Patterns/Patterns.java
+++ src/Patterns/Patterns.java
@@ -145,6 +145,20 @@ public class Patterns {
         //     System.out.println();
         // }
 
+        //   Number 2
+        // 54321
+        // 4321
+        // 321
+        // 21
+        // 1
+
+        for(int i=rows;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
 
     }
 
