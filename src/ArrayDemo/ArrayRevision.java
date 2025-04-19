53fbd6761a8da09d665b77e8d060582f242893d7
ARUN-M-S <arunms96official@gmail.com>
2025-04-19 17:11:18 +0530

    Revision binary search

--- src/ArrayDemo/ArrayRevision.java
+++ src/ArrayDemo/ArrayRevision.java
@@ -13,9 +13,29 @@ public class ArrayRevision {
         }
         return -1;
     }
+   
+    static int binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
         int [] arr={1,4,2,5,6,8,9,03};
         int result = linerSearch(arr, 9);
+
+        int[] nums = {1, 3, 5, 7, 9, 12, 17};
+int target = 9;
         System.out.println(result);
     }
 }
