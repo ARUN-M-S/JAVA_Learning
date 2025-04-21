c5d884e709291c9a6b188d468e7e8eaf9db5a6d2
ARUN-M-S <arunms96official@gmail.com>
2025-04-21 16:32:10 +0530

    searchInRotatedarray

--- src/ArrayDemo/ArrayRevision.java
+++ src/ArrayDemo/ArrayRevision.java
@@ -45,12 +45,58 @@ static void moveZero(int[] nums){
         i++;
     }
 }
-    public static void main(String[] args) {
+  
+
+// static int firstBadVersion(int n) {
+// int low=0;
+// int high = n;
+
+// while(low<high){
+//     int mid = low+ (high-low)/2;
+//     if(isBadVersion(mid)){
+//         high = mid;
+//     }else{
+//         low= mid+1;
+//     }
+// }
+// return low;
+// }
+
+static int searchInRotated(int[] nums,int target){
+    int low = 0;
+    int high = nums.length -1;
+
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+
+        if(nums[low] < nums[mid]  ){
+            if(target > nums[low] && target <=nums[mid]){
+
+                high= mid;
+            }else{
+                low= mid+1;
+            }
+
+        }else{
+            if(target > nums[mid] && target <nums[high]){
+                low= mid+1;
+            }else{
+                high= mid;
+            }
+
+        }
+    }
+    return -1;
+}
+public static void main(String[] args) {
         int [] arr={1,4,2,5,6,8,9,03};
         int result = linerSearch(arr, 9);
 
-        int[] nums = {1, 3, 5, 7, 9, 12, 17};
-int target = 9;
-        System.out.println(result);
+        int[] nums = {4,5,6,7,0,1,2};
+         int target = 7;
+        System.out.println(searchInRotated(nums,target));
     }
 }
